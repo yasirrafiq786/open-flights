8225fe03d2a5f3fc6c47821c829eb9be5cc6c916
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-23 21:47:00 +1000

    Added Data from api

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,13 +1,33 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Airlines = () => {
-    const [airlines, setAirlines] = useState([]);
+  const [airlines, setAirlines] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    axios
+      .get('/api/v1/airlines')
+      .then((response) => {
+        setAirlines(response.data.data);
+      })
+      .catch((response) => console.log(response));
+  }, [airlines.length]);
 
-    },[]);
+  const list = airlines.map((airline) => {
+    return <li key={airline.attributes.name}>{airline.attributes.name}</li>;
+  });
 
-  return <div>Airlines Index</div>;
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest & Unbiased Reviews</div>
+      </div>
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Airlines;
