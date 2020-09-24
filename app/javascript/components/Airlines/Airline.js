40a44859debacbb72b3a75b0d46a127fac521a57
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-24 16:20:47 +1000

    styled components

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    border: 1px solid #efefef;
+    background: #fff;
+`;
+const AirlineLogo = styled.div`
+    width: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    padding-top: 10px;
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid #efefef
+    }
+`;
+const AirlineName = styled.div`
+    padding: 20px 0 10px 0;
+`;
+const LinkWrapper = styled.div`
+    margin: 30px 0 20px 0;
+    height: 50px;
+    a {
+        color: #fff;
+        background: #000;
+        border-radius: 4px;
+        padding: 10px 50px;
+        border: 1px solid #000;
+        width: 100%;
+        text-decoration: none
+    }
+`;
 
 const Airline = (props) => {
-    return (
-        <div className="card">
-            <div className="airline-logo">
-                <img src={props.attributes.image_url} alt={props.attributes.name} />
-            </div>
-            <div className="airline-name">{props.attributes.name}</div>
-            <div className="airline-score">{props.attributes.avg_score}</div>
-            <div className="airline-link">
-                <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <Card>
+      <AirlineLogo>
+        <img src={props.attributes.image_url} alt={props.attributes.name} />
+      </AirlineLogo>
+      <AirlineName>{props.attributes.name}</AirlineName>
+      <div className="airline-score">{props.attributes.avg_score}</div>
+      <LinkWrapper>
+        <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
+      </LinkWrapper>
+    </Card>
+  );
+};
 
 export default Airline;
