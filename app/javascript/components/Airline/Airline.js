e941476783a2daa4b3aae00bec4aaa4cd8a203c1
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-24 18:36:29 +1000

    added style to header component

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,10 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Header from './Header';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+  &:last-child {
+    background: #000;
+  }
+`;
+const Main = styled.div`
+  padding-left: 50px;
+`;
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({});
   const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const slug = props.match.params.slug;
@@ -12,21 +32,31 @@ const Airline = (props) => {
 
     axios
       .get(url)
-      .then((response) => setAirline(response.data))
+      .then((response) => {
+        setAirline(response.data);
+        setLoaded(true);
+      })
       .catch((response) => console.log(response));
   }, []);
 
   return (
-      <div className="wrapper">
-          <div className="column">
-              <Header />
-              <div className="reviews"></div>
-          </div>
-          <div className="column">
-              <div className="review-form">Review Form</div>
-          </div>
-      </div>
-  )
+    <Wrapper>
+      <Column>
+        <Main>
+          {loaded && (
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          )}
+          <div className="reviews"></div>
+        </Main>
+      </Column>
+      <Column>
+        <div className="review-form">Review Form</div>
+      </Column>
+    </Wrapper>
+  );
 };
 
 export default Airline;
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0;
+
+  img {
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+
+  h1 {
+    font-size: 30px;
+  }
+`;
+const TotalReviews = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+`;
+const TotalOutOf = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+  font-weight: bold;
+`;
 
 const Header = (props) => {
-    return (
-        <div className="wrapper">
-            <h1><img src=""/>Airline Image</h1>
-            <div>
-              <div className="totalReviews"></div>
-              <div className="starRating"></div>
-              <div className="totalOutOf"></div>  
-            </div>
-        </div>
-    )
-}
+  const {name, image_url, avg_score} = props.attributes;
+  const total = props.reviews.length;
+  return (
+    <Wrapper>
+      <h1>
+        <img src={image_url} alt={name} />
+        {name}
+      </h1>
+      <div>
+        <TotalReviews>{total} User Reviews</TotalReviews>
+        <div className="starRating"></div>
+        <TotalOutOf>{avg_score} of of 5</TotalOutOf>
+      </div>
+    </Wrapper>
+  );
+};
 
 export default Header;
